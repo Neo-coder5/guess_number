9d118430e82385994f458bc2188c7a855b279a9d
Elmurod <elmurodmarimov7655@gmail.com>
2026-09-10 23:55:36 +0500

    introduction of rules

---
+++ guessing.py
@@ -0,0 +1,9 @@
+import random
+
+attempts = 7
+
+
+def display_rules():
+    print("Welcome to Number Guessing Game!\n")
+    print("I have selected a number between 1 and 100.")
+    print("You have 7 attempts to guess it.\n")
